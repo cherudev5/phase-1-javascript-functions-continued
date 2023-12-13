34360a15337871c6fa7487ddab5dc47c801fe2cd
David <cheruiyotscripts@gmail.com>
2023-12-13 13:59:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(funActivity="roller-skate"){
+  return`This Saturday, I want to ${funActivity}!`;
+  }
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("bathe my dog"));
+
+const mondayWork=function(work="go to the office"){
+    return`This Monday, I will ${work}`;
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(flair = "*") {
+        return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+    
+  } 
+const encouragingPromptFunction = wrapAdjective("!!!");
